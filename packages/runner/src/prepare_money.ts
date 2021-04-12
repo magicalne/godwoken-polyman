a51bc23f99814902273828ce6954e3b7f526b682
root <root@vmi511944.contaboserver.net>
2021-04-12 10:34:45 +0200

    fix prepare money capacity not eough

--- packages/runner/src/prepare_money.ts
+++ packages/runner/src/prepare_money.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { Api } from "./api";
 import serverConfig from "../configs/server.json";
 import gpConfig from "../configs/config.json";
+import { asyncSleep } from "./util";
 
 const _indexer_path = path.resolve(__dirname, "../db/ckb-indexer-data");
 
@@ -12,18 +13,30 @@ const miner_ckb_devnet_addr = serverConfig.miner_ckb_devnet_addr;
 const user_ckb_devnet_addr = serverConfig.user_ckb_devnet_addr;
 const user_account_init_amount = BigInt(serverConfig.user_account_init_amount);
 
+console.log("start..");
+const api = new Api(ckb_rpc, godwoken_rpc, _indexer_path);
+api.syncLayer1();
+
+var retry = 0;
+
 export const run = async () => {
-  console.log("start..");
-  const api = new Api(ckb_rpc, godwoken_rpc, _indexer_path);
   try {
-    await api.syncLayer1();
+    retry++;
+    await api.syncToTip();
     await api.giveUserLayer1AccountSomeMoney(miner_ckb_devnet_addr, miner_private_key, user_ckb_devnet_addr, user_account_init_amount);
     console.log(`prepared money.`);
     console.log(`finished~`);
     process.exit(0);
   } catch (e) {
     console.error(e);
-    process.exit(1);
+    if(retry < 10){
+      asyncSleep(3000);
+      console.log(`retry...${retry}`);
+      run();
+    }else{
+      console.error(`failed to prepare money.`);
+      process.exit(1);
+    }
   }
 };
 
