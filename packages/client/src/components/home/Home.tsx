928b2410878cfbbbbf019ca18552c1ce65b5e5d1
RetricSu <inneverland2013@gmail.com>
2021-05-08 08:21:33 +0200

    to should be 0x when deploy contract

--- packages/client/src/components/home/Home.tsx
+++ packages/client/src/components/home/Home.tsx
@@ -255,25 +255,25 @@ function Home() {
 
       var signature;
       try {
+
         const transactionParameters = {
-          nonce: '0x00', // ignored by MetaMask
-          gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
+          nonce: '0x0', // ignored by MetaMask
+          gasPrice: '0x9184e72a000', // customizable by user during MetaMask confirmation.
           gas: '0x2710', // customizable by user during MetaMask confirmation.
-          to: '0x0000000000000000000000000000000000000000', // Required except during contract publications.
+          to: '0x', // Required except during contract publications.
           from: window.ethereum.selectedAddress, // must match user's active address.
           value: '0x00', // Only required to send ether to the recipient from the initiating external account.
           data: contractCode, // Optional, but used for defining smart contract creation and interaction.
           chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
         };
+
         const txHash = await window.ethereum.request({
           method: 'eth_sendTransaction',
           params: [transactionParameters],
         });
 
-       // signature = await window.ethereum.request({
-       //   method: 'eth_sign',
-       //   params: [window.ethereum.selectedAddress, data.message],
-       // }); 
+        console.log(`txHash: ${txHash}`);
+
       } catch (error) {
         console.log(error);
         return notify(`could not finished signing process. \n\n ${JSON.stringify(error)}`);
