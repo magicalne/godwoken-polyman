13f777dc555aad80b409c2ead33fbc58593a3857
RetricSu <inneverland2013@gmail.com>
2021-06-02 18:52:39 +0800

    mv example configs

--- scripts/tool.sh
+++ scripts/tool.sh
@@ -1,5 +1,5 @@
 copy_configs_if_not_exist(){
-    [ ! -e packages/runner/configs/config.toml ] && cp packages/runner/configs/config.toml.example packages/runner/configs/config.toml;
-    [ ! -e packages/runner/configs/godwoken_config.json ] && cp packages/runner/configs/godwoken_config.json.example packages/runner/configs/godwoken_config.json; 
-    [ ! -e packages/runner/configs/scripts-deploy-result.json ] && cp packages/runner/configs/scripts-deploy-result.json.example packages/runner/configs/scripts-deploy-result.json; 
+    [ ! -e packages/runner/configs/config.toml ] && cp packages/runner/configs/examples/config.toml.example packages/runner/configs/config.toml || echo 'do nothing'; 
+    [ ! -e packages/runner/configs/godwoken_config.json ] && cp packages/runner/configs/examples/godwoken_config.json.example packages/runner/configs/godwoken_config.json || echo 'do nothing'; 
+    [ ! -e packages/runner/configs/scripts-deploy-result.json ] && cp packages/runner/configs/examples/scripts-deploy-result.json.example packages/runner/configs/scripts-deploy-result.json || echo 'do nothing'; 
 }
