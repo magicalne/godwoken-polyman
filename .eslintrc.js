8c731e06a298b7c6132a488edaa7119284900d6c
RetricSu <inneverland2013@gmail.com>
2021-10-19 11:48:38 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
-
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "sourceType": "module"
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
   },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "env": {
-    "browser": true,
-    "es2020": true
+  plugins: ["@typescript-eslint"],
+  env: {
+    browser: true,
+    es2020: true,
   },
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  "rules": {
+  rules: {
     "no-var": "error",
-    "@typescript-eslint/no-unused-vars": ["error"]
-  }
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_|^args$",
+        varsIgnorePattern: "^_",
+      },
+    ],
+  },
 };
